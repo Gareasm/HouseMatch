095865e31c4c9a0d5a09fe39a857a6e5b33ecd15
Gareasm <gareasmayorga@ufl.edu>
2026-03-23 18:08:19 -0400

    Merge branch 'main' into KAN-18-7-ui-register-layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from './components/Register'; //todo standardize component and page locations 
+import Feed from "./pages/Feed";
+import SongDetail from "./pages/SongDetail";
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/feed"
+          element={
+            <AuthGuard>
+              <Feed />
+            </AuthGuard>
+          }
+        />
+        <Route
+          path="/songs/:id"
+          element={
+            <AuthGuard>
+              <SongDetail />
+            </AuthGuard>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
